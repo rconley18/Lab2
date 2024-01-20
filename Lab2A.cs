c94938e0d45f2bcec848c26ea0d05154f4ccafe4
rconley18 <157246229+rconley18@users.noreply.github.com>
2024-01-20 15:19:09 -0500

    Add files via upload

---
+++ Lab2A.cs
@@ -0,0 +1,31 @@
+﻿/* Class: CSE 1321L
+Section: W1
+Term: Spring 24
+Instructor: Manosmi Gundu
+Name: Robert Conley
+Lab#: Lab2A 
+*/
+class Lab2A
+{
+    public static void Main(string[] args)
+    {
+        //define strings
+        string name;
+        string name2;
+        string verb;
+        string adverb;
+        //User Input to Create the Mad Lib
+        Console.WriteLine("Enter a name:");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter another name:");
+        name2 = Console.ReadLine();
+        Console.WriteLine("Enter a verb:");
+        verb = Console.ReadLine();
+        Console.WriteLine("Enter an adverb:");
+        adverb = Console.ReadLine();
+
+        //Print the mad lib
+        string output = String.Format("Once upon a time there was a person named {0} who had a child named {1}. This child would {2} {3} while singing to strangers.", name, name2, verb, adverb);
+        Console.WriteLine(output);
+    }
+}
---
+++ Lab2B.cs
@@ -0,0 +1,65 @@
+﻿/* Class: CSE 1321L
+ * Section: W1
+ * Term: Spring 24
+ * Instructor: Manosni Gundu
+ * Name: Robert Conley
+ * Lab: 2B
+ */
+using System;
+
+class Lab2B
+{
+    static void Main()
+    {
+        PrintShape();
+    }
+
+    static void PrintShape()
+    {
+        int size = 5;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++)
+            {
+                if (k % 2 == 0)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("_");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        for (int i = size - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++)
+            {
+                if (k % 2 == 0)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("_");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
